64d376ec6de77819720394733ac13b97451eed9c
Mariana Santos <maririb51@gmail.com>
2022-06-29 11:36:57 +0100

    Desafio-combate

---
+++ src/application/Program.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Champion;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite os dados do primeiro campeâo: ");
+		System.out.print("Nome: ");
+		String name = sc.nextLine();
+		System.out.print("Vida inicial: ");
+		int life = sc.nextInt();
+		System.out.print("Ataque: ");
+		int attack = sc.nextInt();
+		System.out.print("Armadura:");
+		int armor = sc.nextInt();
+		Champion champion1 = new Champion(name, life, attack, armor);
+
+		System.out.println();
+
+		System.out.println("Digite os dados do segundo campeâo: ");
+		System.out.print("Nome: ");
+		sc.nextLine();
+		name = sc.nextLine();
+		System.out.print("Vida inicial: ");
+		life = sc.nextInt();
+		System.out.print("Ataque: ");
+		attack = sc.nextInt();
+		System.out.print("Armadura:");
+		armor = sc.nextInt();
+		Champion champion2 = new Champion(name, life, attack, armor);
+
+		System.out.println();
+
+		System.out.print("Quantos turnos você deseja executar? ");
+		int rounds = sc.nextInt();
+
+		for (int i = 0; i < rounds; i++) {
+			champion1.takeDamage(champion2);
+			champion2.takeDamage(champion1);
+
+			System.out.println();
+			System.out.println("Resultado do turno " + (i + 1) + ":");
+			System.out.println(champion1.status());
+			System.out.println(champion2.status());
+
+			if (champion1.getLife() == 0 || champion2.getLife() == 0) {
+				i = rounds;
+			}
+		}
+
+		System.out.println();
+		System.out.println("FIM DO COMBATE");
+
+		sc.close();
+
+	}
+
+}
